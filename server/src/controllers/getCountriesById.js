992ed2fa0ef30610842e0a2414b2fbf1e988ef4e
Hernan Parino <you@example.com>
2023-08-26 20:36:14 -0300

    agrego las actividades a los paises por ID

--- server/src/controllers/getCountriesById.js
+++ server/src/controllers/getCountriesById.js
@@ -1,19 +1,29 @@
-const {Country, Activiti} = require("../db");
+const {Country, Activities} = require("../db");
 
-const getCountriesById =  async (id) => {
-    console.log("id ",id);
-    try { 
-        const countrie = await Country.findOne({ where: { id: id }})
-        if (countrie !== null) {
-            // agrego las actividades del pais
-            
-            return countrie;
+const getCountriesById = async (id) => {
+    console.log("id ", id);
+    try {
+        const country = await Country.findOne({
+            where: { id: id },
+            include: {
+                model: Activities,
+                attributes: ['nombre', 'dificultad', 'duracion', 'temporada'],
+                through: {
+                    attributes: [] // Excluir los atributos de la tabla de enlace
+                }
+            }
+        });
+
+        if (country !== null) {
+            return country;
         }
+
         return "error";
-    } catch(error) {
-            return ( error.message)
-        }
+    } catch (error) {
+        return error.message;
+    }
 };
+
 
 
 module.exports = {
